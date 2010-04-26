06d27c58c481c18dc5286e6abc87f974434d36b6
Tobias Crawley <tcrawley@gmail.com>
2010-04-25 22:58:57 -0400

    Fix busted growl support.

--- lib/open_exception/growl_support.rb
+++ lib/open_exception/growl_support.rb
@@ -1,9 +1,17 @@
 module OpenException
   module GrowlSupport
+    def self.included(base)
+      if !base.instance_methods.include?(:open_without_growl)
+        base.send(:alias_method, :open_file_without_growl, :open_file)
+        base.send(:alias_method, :open_file, :open_file_with_growl)
+      end
+    end
+    
     protected
-    def open_file(file, line)
-      growl_notify(file_line) if File.readable?(file)
-      super
+    def open_file_with_growl(file, line)
+      puts 'growl open file'
+      growl_notify(file, line) if File.readable?(file)
+      open_file_without_growl(file, line)
     end
     
     def growl_notify(file, line)
@@ -16,10 +24,10 @@ module OpenException
     end
     
     def growl_message(file, line)
-      msg = "Opening #{file}:#{line}"
+      msg = "Exception: #{exception.message} at #{exception.backtrace.first}\nOpening #{file}:#{line}"
       msg << " in #{options[:open_with]}" if options[:open_with].is_a?(Symbol)
     end
   end
 end
 
-OpenException::ExceptionOpener.send(:include, OpenException::GrowlSupport)
+OpenException::ExceptionOpener.send(:include, OpenException::GrowlSupport) 
