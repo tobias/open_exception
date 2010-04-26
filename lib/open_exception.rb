faba32cd0d2957a0b3a85a176ed74a506e0a2e41
Tobias Crawley <tcrawley@gmail.com>
2010-04-25 22:58:21 -0400

    Added init message.

--- lib/open_exception.rb
+++ lib/open_exception.rb
@@ -1,9 +1,14 @@
 require File.dirname(__FILE__) + "/open_exception/open_exception"
 require File.dirname(__FILE__) + "/open_exception/rails" if defined?(ActionController)
 
+init_message = "** open_extension initialized "
+
 begin
   require 'growl'
   require File.dirname(__FILE__) + "/open_exception/growl_support"
+  init_message << "with growl support"
 rescue LoadError
   #ignore
 end
+
+puts init_message
